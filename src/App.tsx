2c6e3d0bd0dd451f9bb1882cf5881333bdbc8ffe
ryokryok <mrozisan@gmail.com>
2021-05-18 04:22:46 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
 import React, { Dispatch } from "react"
 import "./App.css"
 
-import { useSelector, useDispatch } from "react-redux"
+import { useSelector, useDispatch, TypedUseSelectorHook } from "react-redux"
 import { CounterAction, AppState } from "./redux"
 
-const { value } = useSelector((state: AppState) => state.counter)
-const countDispatch = useDispatch<Dispatch<CounterAction>>()
-const handleIncrement = () => countDispatch({ type: "counter/increment" })
-const handleDecrement = () => countDispatch({ type: "counter/decrement" })
-const handleReset = () => countDispatch({ type: "counter/reset" })
+const useTypedSelector: TypedUseSelectorHook<AppState> = useSelector
 
 function App() {
+  const { value } = useTypedSelector((state) => state.counter)
+  const countDispatch = useDispatch<Dispatch<CounterAction>>()
+  const handleIncrement = () => countDispatch({ type: "counter/increment" })
+  const handleDecrement = () => countDispatch({ type: "counter/decrement" })
+  const handleReset = () => countDispatch({ type: "counter/reset" })
+
   return (
     <div className="App">
-      <p>Hello Vite + React!</p>
+      <h1>Hello Vite + React!</h1>
       <div>
         <h2>Count: {value}</h2>
-        <button onClick={handleIncrement}>+</button>
-        <button onClick={handleDecrement}>-</button>
-        <button onClick={handleReset}>reset</button>
+        <MemoizedButton buttonAction={handleIncrement} displayText={"+"} />
+        <MemoizedButton buttonAction={handleDecrement} displayText={"-"} />
+        <MemoizedButton buttonAction={handleReset} displayText={"reset"} />
       </div>
     </div>
   )
 }
+
+type ButtonProps = {
+  buttonAction: () => void
+  displayText: string
+}
+
+function CounterButton({ buttonAction, displayText }: ButtonProps) {
+  return <button onClick={buttonAction}>{displayText}</button>
+}
+
+const MemoizedButton = React.memo(CounterButton)
 
 export default App
