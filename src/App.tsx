48c9e01d07bf0b6390a76ced68b19d92cb102f8b
ryokryok <mrozisan@gmail.com>
2021-05-22 02:16:58 +0900

    feature async redux state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React, { Dispatch } from "react"
+import React, { Dispatch, useEffect } from "react"
 import "./App.css"
 
 import { useSelector, useDispatch, TypedUseSelectorHook } from "react-redux"
-import { CounterAction, AppState } from "./redux"
+import { CounterAction, ApiAction, AppState, apiClient } from "./redux"
 
 const useTypedSelector: TypedUseSelectorHook<AppState> = useSelector
+
+function useFetchApi() {
+  const { ip } = useTypedSelector((state) => state.api)
+  const apiDispatch = useDispatch<Dispatch<ApiAction>>()
+
+  const fetchAction = async () => {
+    apiDispatch({ type: "ipify/start" })
+    try {
+      const response = await apiClient()
+      apiDispatch({ type: "ipify/success", response: response })
+    } catch (error) {
+      apiDispatch({ type: "ipify/failed" })
+    }
+  }
+  useEffect(() => {
+    fetchAction()
+  }, [])
+
+  return ip
+}
 
 function App() {
   const { value } = useTypedSelector((state) => state.counter)
@@ -12,6 +32,7 @@ function App() {
   const handleIncrement = () => countDispatch({ type: "counter/increment" })
   const handleDecrement = () => countDispatch({ type: "counter/decrement" })
   const handleReset = () => countDispatch({ type: "counter/reset" })
+  const ip = useFetchApi()
 
   return (
     <div className="App">
@@ -21,6 +42,9 @@ function App() {
         <MemoizedButton buttonAction={handleIncrement} displayText={"+"} />
         <MemoizedButton buttonAction={handleDecrement} displayText={"-"} />
         <MemoizedButton buttonAction={handleReset} displayText={"reset"} />
+      </div>
+      <div>
+        <h2>IP Address: {ip}</h2>
       </div>
     </div>
   )
--- src/redux.ts
+++ src/redux.ts
@@ -1,4 +1,5 @@
-import { combineReducers, createStore } from "redux"
+import { combineReducers, createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
 
 export type CounterAction = {
   readonly type: "counter/increment" | "counter/decrement" | "counter/reset"
@@ -28,10 +29,52 @@ function counterReducer(
   }
 }
 
+type ApiResponse = {
+  ip: string
+}
+
+type ApiActionStart = {
+  readonly type: "ipify/start"
+}
+
+type ApiActionSuccess = {
+  readonly type: "ipify/success"
+  response: ApiResponse
+}
+
+type ApiActionFailed = {
+  readonly type: "ipify/failed"
+}
+
+export type ApiAction = ApiActionStart | ApiActionSuccess | ApiActionFailed
+
+export const initialApiState: ApiResponse = {
+  ip: "",
+}
+
+export function apiReducer(prevState = initialApiState, action: ApiAction) {
+  switch (action.type) {
+    case "ipify/start":
+      return prevState
+    case "ipify/success":
+      return action.response
+    case "ipify/failed":
+      return prevState
+    default:
+      return prevState
+  }
+}
+
 const rootReducer = combineReducers({
   counter: counterReducer,
+  api: apiReducer,
 })
+
+export async function apiClient(): Promise<ApiResponse> {
+  const response = await fetch("https://api.ipify.org/?format=json")
+  return response.json()
+}
 
 export type AppState = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(thunk))
